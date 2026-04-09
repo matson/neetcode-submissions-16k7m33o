07986e7ec89302cb56c517095ab701626bbdb628
Matson <madison.k.adams@gmail.com>
2026-04-09 16:38:35 -0400

    Add: insert-into-a-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-0.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        guard let root = root else{
+            return TreeNode(val) 
+        }
+
+        if val > root.val{
+            root.right = insertIntoBST(root.right, val)
+        }else if val < root.val{
+            root.left = insertIntoBST(root.left, val)
+        }
+        return root 
+    }
+}
