c2b168466acbb00ffa209d3339065c89a9637ecc
Matson <madison.k.adams@gmail.com>
2026-04-21 15:08:51 -0400

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var maxSum = nums[0]
+        var curSum = 0
+        
+        for n in nums{
+            curSum = max(curSum, 0)
+            curSum += n
+            maxSum = max(maxSum, curSum)
+        }
+        return maxSum
+    }
+}
