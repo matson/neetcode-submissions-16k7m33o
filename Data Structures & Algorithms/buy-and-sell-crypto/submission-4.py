c4acc78135f635ac39c619b2628309abe40dcc79
Matson <madison.k.adams@gmail.com>
2026-05-03 17:19:59 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        # sliding window 
+
+
+        max_profit = 0 
+        L = 0 
+        
+        for R in range(len(prices)):
+            profit = prices[R] - prices[L]
+            if prices[R] < prices[L]:
+                L = R
+            else:
+                max_profit = max(max_profit, profit)
+        return max_profit 
+        
