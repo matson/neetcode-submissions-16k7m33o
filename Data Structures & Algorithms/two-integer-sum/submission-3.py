d2eda757dc9b04e3747e40544cf82b14fe9261d2
Matson <madison.k.adams@gmail.com>
2026-08-24 15:12:18 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # two pointers O(n^2)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+        return [] 
+        
